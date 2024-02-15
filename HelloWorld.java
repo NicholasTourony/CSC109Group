7b8569dc5e0a8986b5c949a2c0e4a003bd4e4698
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-02-14 19:24:45 -0500

    added polish hello world text

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        System.out.println("Witaj świecie!");
         for (int counter = 0; counter < 10; counter++){
             System.out.println(counter);
         }
