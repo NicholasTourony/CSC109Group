c58d2e721ec1e01e7c39d347c61a23845dafaf67
PeterZegarek <petrzegarek@gmail.com>
2024-02-14 19:21:46 -0500

    Added simple for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        for (int counter = 0; counter < 10; counter++){
+            System.out.println(counter);
+        }
     }
 }
